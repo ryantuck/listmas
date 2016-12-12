a621ac386c4fa78a2264c7a042fda0bf444a10d4
Ryan Tuck <ryntck@gmail.com>
2016-12-11 23:11:28 -0500

    handle blank strings for item attributes

--- backend/lambda/functions/put_list/main.py
+++ backend/lambda/functions/put_list/main.py
@@ -5,9 +5,16 @@ import boto3
 
 def put_list(event, context):
 
-    print event
-
+    # append updated timestamp
     event.update({'updated': str(datetime.datetime.utcnow())})
+
+    # dynamodb can't handle blank strings, so convert to nulls
+    for item in event['items']:
+        for k,v in item.items():
+            if v == '':
+                item[k] = None
+
+    print event
 
     response = boto3.resource('dynamodb').Table('lists').put_item(
         Item=event,
