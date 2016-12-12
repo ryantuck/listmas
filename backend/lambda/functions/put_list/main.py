386ba888698fa3641adc45296d54fc08c9a8dc87
Ryan Tuck <ryntck@gmail.com>
2016-12-11 19:37:57 -0500

    add updated timestamp on writes

--- backend/lambda/functions/put_list/main.py
+++ backend/lambda/functions/put_list/main.py
@@ -1,7 +1,13 @@
+import datetime
+
 import boto3
 
 
 def put_list(event, context):
+
+    print event
+
+    event.update({'updated': str(datetime.datetime.utcnow())})
 
     response = boto3.resource('dynamodb').Table('lists').put_item(
         Item=event,
